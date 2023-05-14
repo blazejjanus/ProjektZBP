f329ef3deff3f2ec1d3a558121ee9f6f6eeabd32
Błażej Janus <blazejjanus@gmail.com>
2023-05-14 14:59:07 +0200

    DataStructures1

---
+++ ZBP/Data/Predictions.cs
@@ -0,0 +1,13 @@
+﻿using ZBP.Attributes;
+using ZBP.Enums;
+
+namespace ZBP.Data {
+    public class Predictions {
+        [Factor("WIG", Quantity.Percent)]
+        public double Wig { get; set; }
+        [Factor("WIG-20", Quantity.Percent)]
+        public double Wig20 { get; set; }
+        [Factor("WIG-30", Quantity.Percent)]
+        public double Wig30 { get; set; }
+    }
+}
---
+++ ZBP/Data/StockRecord.cs
@@ -0,0 +1,14 @@
+﻿namespace ZBP.Data {
+    public class StockRecord {
+        public DateOnly Date { get; set; }
+        public double Growth { get; set; }
+
+        public static List<StockRecord> ReadCSV(string filename) {
+            var result = new List<StockRecord>();
+            if(!File.Exists(filename)) {
+                throw new Exception("File not exists!");
+            }
+            return result;
+        }
+    }
+}
